16cbf269ae2900b364a798a1775084a341a16f1c
Fernando Bonanno <fbonanno@prismamp.com>
2023-01-27 11:03:58 -0300

    init

---
+++ src/main/java/com/misTrabajos/clientes/ClientesApplication.java
@@ -0,0 +1,13 @@
+package com.misTrabajos.clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesApplication.class, args);
+	}
+
+}
