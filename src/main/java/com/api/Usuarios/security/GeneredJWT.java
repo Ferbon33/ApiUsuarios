c47559d78d3f669d923ee53a4deb16de7ab4f21c
Fernando Bonanno <fbonanno@prismamp.com>
2023-09-24 12:44:14 -0300

    se agrego jwt

--- src/main/java/com/api/Usuarios/security/GeneredJWT.java
+++ src/main/java/com/api/Usuarios/security/GeneredJWT.java
@@ -8,29 +8,37 @@ import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.stereotype.Service;
 
 @Component
 public class GeneredJWT {
-/*/
-    public String Create (){
-        SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
-        String usuarioId = "123";
-        String nombreUsuario = "ejemploUsuario";
 
-        Date fechaEmision = new Date();
-        Date fechaExpiracion = new Date(fechaEmision.getTime() + 3600000);
+    public String generedJWT(String username) {
+        String secretKey = "none";
+        List<GrantedAuthority> grantedAuthorities = AuthorityUtils
+                .commaSeparatedStringToAuthorityList("ROLE_USER");
 
-        String jwt = Jwts.builder()
-                .setSubject(usuarioId)
-                .claim("nombreUsuario", nombreUsuario)
-                .setIssuedAt(fechaEmision)
-                .setExpiration(fechaExpiracion)
-                .signWith(secretKey)
-                .compact();
+        String token = Jwts
+                .builder()
+                .setSubject(username)
+                .claim("authorities",
+                        grantedAuthorities.stream()
+                                .map(GrantedAuthority::getAuthority)
+                                .collect(Collectors.toList()))
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 600000))
+                .signWith(SignatureAlgorithm.HS512,
+                        secretKey.getBytes()).compact();
 
-        return jwt;
+        return "Bearer " + token;
     }
-    /*/
 
-}
+    }
+
+
+
